b4cfb24bf595c2d516179029897a2a749a2f6a2f
sarfaa <regki@mail.bg>
2015-02-24 18:05:42 +0200

    READY to APPLY

--- 3-CSV-Querry-Language/csv_querry-language.py
+++ 3-CSV-Querry-Language/csv_querry-language.py
@@ -65,6 +65,24 @@ def sum_column():
 	else:
 		print "Can not sum this column."
 
+def show_command():
+	print ", ".join(input_file[0])
+
+def find_all_rows_containing_substring():
+	substring = commands[1]
+	rows_to_print = [input_file[0]]
+
+	for i in range(1, len(input_file)):
+		for col in input_file[i]:
+			if substring in col:
+				rows_to_print.append(input_file[i])
+				break
+	result = "|"
+	for row in rows_to_print:
+		result += "|".join(row)
+		result += '|\n'
+	print result
+
 def find_columns(end_of_commands):
 	columns = []
 	for i in range(1, end_of_commands):
@@ -78,35 +96,42 @@ def find_limit(index_of_limit):
 		return commands[index_of_limit + 1]
 
 while True:
-	querry = raw_input("querry>")
-	querry = str(querry)
+	input_line = raw_input("querry>")
+	input_line = str(input_line)
 
 	while True:
 		p = re.compile(ur'[,|\s]?([A-z0-9\-]+)+[,|\s]?', re.IGNORECASE)
+		commands = re.findall(p, input_line)
 
-		 
-		commands = re.findall(p, querry)
 		if not commands:
 			print("Please enter something!")
 			break
-		elif commands[0] == 'SELECT':
-			if len(commands) == 1:
-				print('No enought SELECT parameters')
-				break
-			else:
-				if 'LIMIT' in commands:
-					select_limit()
+		else:
+			querry = commands[0]
+			if querry == 'SELECT':
+				if len(commands) == 1:
+					print('No enought SELECT parameters')
 					break
 				else:
-					select_without_limit()
+					if 'LIMIT' in commands:
+						select_limit()
+						break
+					else:
+						select_without_limit()
+						break
+			elif querry == 'SUM':
+				if len(commands) == 1:
+					print('No enought SUM parameters')
 					break
-		elif commands[0] == 'SUM':
-			if len(commands) == 1:
-				print('No enought SUM parameters')
+				else:
+					sum_column()
+					break
+			elif querry == 'SHOW':
+				show_command()
+				break
+			elif querry == 'FIND':
+				find_all_rows_containing_substring()
 				break
 			else:
-				sum_column()
+				print "Illegal querry!"
 				break
-		else:
-			print "Illegal querry!"
-			break
